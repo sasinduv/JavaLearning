2e17f64d02decf0dbf0551c53760f5c414c741e6
sasinduv <sasindugihan123456@gmail.com>
2026-04-05 17:20:02 +0530

    Day 11 example codes

--- Day11/src/Example.java
+++ Day11/src/Example.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+class Student{
+    String name;
+    int age;
+    Student (String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+}
 
 public class Example {
+    public static void main(String[] args) {
+
+    }
 }
