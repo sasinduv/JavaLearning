cd4079aecc4c0aa7091b912f1494fcf0b7b97c55
sasinduv <sasindugihan123456@gmail.com>
2026-04-07 01:29:13 +0530

    inheritance examples

--- Day13/src/Example.java
+++ Day13/src/Example.java
@@ -1,19 +1,114 @@
-class Animal{
-    public void walk(){
-        System.out.println("I am walking");
+//class Animal{
+//    public void walk(){
+//        System.out.println("I am walking");
+//    }
+//}
+//
+//class Cat extends Animal{}
+//
+//class Dog extends Animal{}
+//
+//
+//class Example {
+//    public static void main(String[] args) {
+//        Cat cat= new Cat();
+//        cat.walk();
+//
+//        new Dog().walk();
+//    }
+//}
+
+//example 2
+/*
+class A {
+    int a=1;
+    public void printA(){
+        System.out.println("printA :"+a);
     }
 }
 
-class Cat extends Animal{}
+class B extends A{
+    int b=1;
+    public void printB(){
 
-class Dog extends Animal{}
-
+        System.out.println("printB :"+b);
+    }
+    public void invokeBoth(){
+        printB();
+        printA();
+    }
+    public void printBoth(){
+        System.out.println("printBoth : "+b);
+        System.out.println("printBoth : "+a);
+    }
+}
 
 class Example {
     public static void main(String[] args) {
-        Cat cat= new Cat();
-        cat.walk();
+        B b = new B();
 
-        new Dog().walk();
+        b.invokeBoth();
+        System.out.println("============");
+        b.printBoth();
+    }
+}
+*/
+//example 3
+
+/*
+class A{
+    int a=1;
+    public void printA(){
+        System.out.println("printA : "+a);
+    }
+    static{
+        System.out.println("static block");
+    }
+}
+
+
+class B extends A{
+    int b=2;
+    public void printB(){
+        System.out.println("printB() :"+b);
+    }
+    static{
+        System.out.println("static block B");
+    }
+}
+
+class Example {
+    public static void main(String[] args) {
+        new B();
+    }
+}        */
+
+//example 4
+class A{
+    static{
+        System.out.println("static block A");
+    }
+}
+
+class B extends A{
+    static{
+        System.out.println("static block B");
+    }
+}
+
+class C extends B{
+    static{
+        System.out.println("static block C");
+    }
+}
+
+class D extends B{
+    static{
+        System.out.println("static block D");
+    }
+}
+class Example {
+    public static void main(String[] args) {
+        new B();
     }
 }
