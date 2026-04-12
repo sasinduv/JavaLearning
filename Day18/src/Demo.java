106464834fdef01d503aee23fa07b712a04659b1
sasinduv <sasindugihan123456@gmail.com>
2026-04-12 10:00:03 +0530

    Day 18 example code about interface

---
+++ Day18/src/Demo.java
@@ -0,0 +1,27 @@
+//class PrintInfo{
+//    Integer integer;
+//    PrintInfo(Integer integer){
+//        this.integer=integer;
+//    }
+//    public void print(){
+//        System.out.println(integer);
+//    }
+//}
+//public class Demo {
+//    public static void main(String[] args) {
+//        Integer integer=new Integer(100);
+//        PrintInfo printInfo=new PrintInfo(integer);
+//        printInfo.print();
+//
+//    }
+//}
+
+
+//abstract class Vehicle{
+//    abstract public void park();
+//    abstract public void stop();
+//}
+
+interface Vehicle{ void park();
+ void stop();
+}
